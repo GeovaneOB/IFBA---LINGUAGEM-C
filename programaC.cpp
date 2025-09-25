93d41406aba81d5fe7af64b248ba87c8b7442947
GeovaneOB <118844613+GeovaneOB@users.noreply.github.com>
2025-09-24 21:34:21 -0300

    Add files via upload

---
+++ programaC.cpp
@@ -0,0 +1,12 @@
+// IFBA - Geovane O.B. | 24.09.25
+
+#include <stdio.h> // Biblioteca responsável por gerenciar as entradas e saídas.
+#include <stdlib.h> // Biblioteca responsável por gerenciar funções utilitárias.
+
+main () { // Meu programa em C:
+	
+	printf ("Meu segundo programa em C!\n"); // Saída
+	
+	getchar(); // Getchar para capturar o último comando do teclado.
+	
+}
